2bf6b96bfcb59fd9ee3d0f9318417a69fe722b91
DESKTOP-QGR187P\x230 <viknox1912@gmail.com>
2016-11-18 14:48:01 +0100

    Added Toolbar

--- app/src/main/java/com/example/viknox/viknoxmems/ActivityMain.java
+++ app/src/main/java/com/example/viknox/viknoxmems/ActivityMain.java
@@ -1,14 +1,22 @@
 package com.example.viknox.viknoxmems;
 
+import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 public class ActivityMain extends AppCompatActivity {
-    private String hello_world = "Konnichiwa";
+    Toolbar toobar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toobar = (Toolbar) findViewById(R.id.id_toolbar);
+        setSupportActionBar(toobar);
+
+
     }
 }
